6c99cb750fd8a8ff06f2442f219542f02ff6221a
Mariano Fernandez <mariano.fernandez@fizzmod.com>
2021-09-01 14:21:15 -0300

    Add tests

--- tests/handlebars-test.js
+++ tests/handlebars-test.js
@@ -1,14 +1,74 @@
 'use strict';
 
-// const assert = require('assert');
+const assert = require('assert');
 
-/* const {
+const {
 	Handlebars,
 	HandlebarsError
-} = require('../lib'); */
+} = require('../lib');
 
 describe('Handlebars', () => {
 
-	// your tests here...
+	context('When must render using markdown helper', () => {
 
+		const template = '<html><body>{{markdown ## Probando}}</body></html>';
+		const templateCompiled = Handlebars.compile(template, 'strict');
+
+		it('Should return template with value in html', () => {
+
+			const values = {
+				title: 'test'
+			};
+
+			assert.strictEqual(templateCompiled(values), '<html><body><h2 id="probando">Probando</h2></body></html>');
+		});
+	});
+
+	context('When must render using lowercase helper', () => {
+
+		const template = '<html><body><h1>{{lowercase title}}</h1></body></html>';
+		const templateCompiled = Handlebars.compile(template, 'strict');
+
+		it('Should return template with value in lowercase', () => {
+
+			const values = {
+				title: 'TEST'
+			};
+
+			assert.strictEqual(templateCompiled(values), '<html><body><h1>test</h1></body></html>');
+		});
+
+		it('Should return template with value in string if value is number', () => {
+
+			const values = {
+				title: 100
+			};
+
+			assert.strictEqual(templateCompiled(values), '<html><body><h1>100</h1></body></html>');
+		});
+	});
+
+	context('When must render using uppercase helper', () => {
+
+		const template = '<html><body><h1>{{uppercase title}}</h1></body></html>';
+		const templateCompiled = Handlebars.compile(template, 'strict');
+
+		it('Should return template with value in uppercase', () => {
+
+			const values = {
+				title: 'test'
+			};
+
+			assert.strictEqual(templateCompiled(values), '<html><body><h1>TEST</h1></body></html>');
+		});
+
+		it('Should return template with value in string if value is number', () => {
+
+			const values = {
+				title: 100
+			};
+
+			assert.strictEqual(templateCompiled(values), '<html><body><h1>100</h1></body></html>');
+		});
+	});
 });
